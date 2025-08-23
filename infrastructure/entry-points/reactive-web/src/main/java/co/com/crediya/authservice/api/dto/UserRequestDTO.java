d6cd79a23771534fd6a9bcb47f7b117e137dac01
William Sanchez <sanchezalvarez124@gmail.com>
2025-08-23 18:43:26 -0500

    feat(user): Añadir anotaciones de JsonProperty a los campos de UserRequestDTO para mejorar la serialización JSON

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/authservice/api/dto/UserRequestDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/authservice/api/dto/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package co.com.crediya.authservice.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 
@@ -8,26 +9,32 @@ import java.util.UUID;
 @Getter
 public class UserRequestDTO {
 
+    @JsonProperty("name")
     @NotBlank
     @Size(max = 100)
     private String name;
 
+    @JsonProperty("email")
     @NotBlank
     @Email
     private String email;
 
+    @JsonProperty("password")
     @NotBlank
     @Size(min = 8)
     private String password;
 
+    @JsonProperty("document_number")
     @NotBlank
     @Size(max = 20)
     private String documentNumber;
 
+    @JsonProperty("phone")
     @NotBlank
-    @Pattern(regexp = "^\\+?\\d{7,15}$")
+    @Pattern(regexp = "^\\+?\\d{7,15}$", message = "El número de teléfono debe tener entre 7 y 15 dígitos y puede empezar con +")
     private String phone;
 
+    @JsonProperty("role_id")
     @NotNull
     private UUID roleId;
 
