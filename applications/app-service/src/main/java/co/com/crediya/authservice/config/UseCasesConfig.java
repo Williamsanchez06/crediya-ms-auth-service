9996eb7dfb27c9061a86919f04323907597b68ba
William Sanchez <sanchezalvarez124@gmail.com>
2025-08-22 23:41:34 -0500

    chore(init): inicializar crediya-ms-auth-service con arquitectura limpia reactiva basada en el scaffold de Bancolombia

---
+++ applications/app-service/src/main/java/co/com/crediya/authservice/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.crediya.authservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.crediya.authservice.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
